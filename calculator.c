3efcfa94e594e18cfe8c0b103b3cb49c5d215223
vibhorg27 <90828247+vibhorg27@users.noreply.github.com>
2022-04-26 01:19:47 -0700

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -55,7 +55,7 @@ int main()
             res = n1 / n2; // divide two numbers  
             printf (" Division of %d and %d is: %.2f", n1, n2, res);  
             break;    
-            printf (" Something is wrong!! Please check the options ");               
+        default: printf (" Something is wrong!! Please check the options ");               
     }  
     return 0;  
 }  
